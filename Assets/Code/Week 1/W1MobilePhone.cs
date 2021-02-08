b8849f5ce00666f68866eaec48dc3911ef8e77f9
Susseroase <jacksonsking@hotmail.co.uk>
2021-02-08 11:58:06 +1100

    middle of class morning

---
+++ Assets/Code/Week 1/W1MobilePhone.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class W1MobilePhone : MonoBehaviour
+{
+    void Start()
+    {
+        //can get touch out of bounds error
+        if (Input.touchCount > 0)
+        {
+            //Because multi touch is supported, you can put this in for loop
+            Touch touch = Input.GetTouch(0);
+
+
+            //positer - never us it, hte position in world
+            //Raw position - 
+            //Delta position - change in pixel betwen frames
+            //Delta time - time passed between the last touch
+            //Tap count - how many time tapped
+            //Touch phase - which point in the touch lifestyle. Begin the first frame, ended when touch is lifted from screen, cnaceld is if there is an error, doesn't happen often but does
+            //Pressure - it's pointless, only iOS has this (it's called force touch)
+            //Touch type: direct (standard), indirect (remote touch, simulated touch), Stylus (touching stylus)
+
+            //both have to do with stylus:
+            //Altitude angle: angle on the Y the stylus is touching. 
+            //AzimuthAngle - x axis. 0 means stylus is parallel
+
+            //radius: fingers, radius of the touch
+            //radius variant -averaged touch radius. You can add this to radius to get the maximum touch size, or minus to the get the smallest touch size.
+
+
+            //Gyroscope
+
+            //gravity: acceleration vector of the frame of turning. How quickly 
+            //rotation rate: same thing but more accurate,the hard value of totalspeed, similar to delta time
+            //update interval: how often it is updating in seconds
+            //acceleration: count number of rotation
+            //
+
+            Input.gyro.enabled = true; //you actually have to turn it on first, it's not on by default.
+            
+
+
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Code/Week 1/W2MobileInput.cs
+++ Assets/Code/Week 1/W2MobileInput.cs
@@ -2,35 +2,69 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+
+//Swipe: there is no native function for swipe
 //What is flick? Flick is taking the first touch, in update frame, monitor it moving until it ends. Then you check the amount of time for this to occur. If it's more than a second, it becomes a swipe.
-//https://github.com/jjhesk/unity-interview/blob/master/Assets/Plugins/EasyTouch/EasyTouch.cs
 
-
-public class MobileInput : MonoBehaviour
+public class W2MobileInput : MonoBehaviour
 {
+
+    
+
+
+    /// <summary>
+    /// have swipe as a class so that we can calculate them sepertately. Have a
+    /// </summary>
+    /// 
     public class Swipe
     {
-        public Vector2 initialPosition = new Vector2();
+        /// <summary>
+        /// List of points along the swipe, added each frame
+        /// </summary>
         public List<Vector2> positions = new List<Vector2>();
-        public int fingerID;
+        /// <summary>
+        /// The position the swipe started from
+        /// </summary>
+        public Vector2 initialPosition = new Vector2();
+        /// <summary>
+        /// The finger id associated with this swipe
+        /// </summary>
+        public int fingerID = 0;
 
         public Swipe(Vector2 _initialPos, int _fingerID)
         {
             initialPosition = _initialPos;
-            positions.Add(_initialPos);
             fingerID = _fingerID;
+            positions.Add(_initialPos);
         }
+
     }
 
-    public static Vector3 FlickDirection { get; private set; } = Vector3.positiveInfinity; // The direciton of the lats flick. Positive infinithy means no flick occured.
-    public static float FlickPower { get; private set; } = float.PositiveInfinity; // How fast or hard the player flicked.
-    public static int SwipeCount => Swipes.Count; // How many swipes are in progress.
 
+    /// <summary>
+    /// The direciton o that the flick last occured, positive infinithy means no flick occured.
+    /// </summary>
+    public static Vector3 FlickDirection { get; private set; } = Vector3.positiveInfinity;
 
-    private static Dictionary<int, Swipe> Swipes = new Dictionary<int, Swipe>(); //Key - fingerID. Value - swipe.
-    private float flickTime = 0; //How long the swipe has been running for.
-    private Vector2 flickOrigin = Vector2.positiveInfinity; //The origin point for the occuring swipe
-    private int initialFinger = -1; //The initialiting finger index
+    /// <summary>
+    /// How hard and far the player flicked. speed.
+    /// </summary>
+    public static float FlickPower { get; private set; } = float.PositiveInfinity;
+    /// <summary>
+    /// The count of how many swipes are in progress
+    /// </summary>
+    public static int SwipeCount => Swipes.Count;
+
+    //Key: finger iD. Value: swipe class
+    private static Dictionary<int, Swipe> Swipes = new Dictionary<int, Swipe>();
+
+    //How long the swipe has been running for.
+    private float flickTime = 0;
+    //The origin point for the occuring swipe
+    private Vector2 flickOrigin = Vector2.positiveInfinity;
+
+    //The initialiting finger index, so that if the player accidentaly pinky touched it, the will aga
+    private int initialFInger = -1;
 
 
     /// <summary>
@@ -38,32 +72,40 @@ public class MobileInput : MonoBehaviour
     /// </summary>
     /// <param name="_index"> The finger ID we are attempting to get the swipe for </param>
     /// <returns> The corresponding swipe if it exists, otherwise null. </returns>
-    public static Swipe GetSwipe(int _index)
+    public static Swipe GetSwipe (int _index)
     {
         Swipe temp;
+        //Assigns swipe if it exists
         Swipes.TryGetValue(_index, out temp);
         return temp;
     }
 
+
+    void Start()
+    {
+
+    }
+
+    //Make sure we
     void Update()
     {
-        if (HasTouch)
+        //Check if any touches, 
+        if (Input.touchCount > 0)
         {
             foreach (Touch touch in Input.touches)
             {
-                //If this is the first touch in the swipe
-                //Flick 
-                if (touch.phase == TouchPhase.Began && flickOriginResetted)
+                //If this is the first touh in the swip 
+                if (touch.phase == TouchPhase.Began && flickOrigin.Equals(Vector2.positiveInfinity))
                 {
                     flickOrigin = touch.position;
-                    initialFinger = touch.fingerId;
+                    initialFInger = touch.fingerId;
                 }
-                else if (touch.phase == TouchPhase.Ended && touch.fingerId == initialFinger && flickTime < 1f)
+                else if (touch.phase == TouchPhase.Ended && touch.fingerId == initialFInger && flickTime < 1f)
                 {
                     CalculateFlick(touch.position);
                 }
 
-                // Swipe storage 
+                // --- Begin swipe storage ---
                 if (touch.phase == TouchPhase.Began)
                 {
                     Swipes.Add(touch.fingerId, new Swipe(touch.position, touch.fingerId));
@@ -78,6 +120,8 @@ public class MobileInput : MonoBehaviour
                     //The swipe has ended so remove it
                     Swipes.Remove(swipe.fingerID);
                 }
+
+                // --- End swipe storage ---
             }
             //There isn't so reset swipe data
             flickTime = Time.deltaTime;
@@ -87,7 +131,6 @@ public class MobileInput : MonoBehaviour
             ResetSwipe();
         }
     }
-
 
     /// <summary>
     /// Calculate the flick
@@ -118,7 +161,4 @@ public class MobileInput : MonoBehaviour
         flickOrigin = Vector2.positiveInfinity;
         flickTime = 0; ;
     }
-
-    bool HasTouch => Input.touchCount > 0;
-    bool flickOriginResetted => flickOrigin == Vector2.positiveInfinity;
 }
