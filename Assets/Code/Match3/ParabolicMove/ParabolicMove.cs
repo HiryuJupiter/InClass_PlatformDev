0d0f7789f099a1abf06835670d189d5a6c89508f
HiryuJupiter <xooase@gmail.com>
2021-03-02 16:22:45 +1100

    end of tue class

--- Assets/Code/Match3/ParabolicMove/ParabolicMove.cs
+++ Assets/Code/Match3/ParabolicMove/ParabolicMove.cs
@@ -6,6 +6,20 @@ public class ParabolicMove : MonoBehaviour
     const float Height = 0.5f;
     public static Vector3 Move(Vector3 startPos, Vector3 endPos, float t)
     {
+        //Vector3 facing = endPos - startPos;
+        //Vector3 relativeUpDir = Quaternion.Euler(0f, 0f, 90f) * facing; //Rotate a vector 90 degrees to the left
+
+        //Debug.DrawRay(startPos, facing, Color.red, 1f);
+        //Debug.DrawRay(startPos, relativeUpDir, Color.green, 1f);
+
+        //float magnitude = facing.magnitude;
+
+        //float x = Mathf.Lerp(0f, magnitude, t);
+
+        //Vector3 scaledLinearPos = new Vector3(x, 0f);
+        //return Quaternion.LookRotation(Vector3.forward, relativeUpDir) * scaledLinearPos;
+
+        //
         Vector3 facing = endPos - startPos;
         float magnitude = facing.magnitude;
 
@@ -13,7 +27,7 @@ public class ParabolicMove : MonoBehaviour
         float y = -4 * Height * x * x + 4 * Height * x;
 
         Vector3 scaledParabolicPos = new Vector3(x * magnitude, y * magnitude);
-        Vector3 relativeUpDir = Quaternion.Euler(0f, 0f, 90f) * facing; //Rotate a vector 90 degrees to the left
+        Vector3 relativeUpDir = Quaternion.Euler(0f, 0f, 90f) * facing.normalized; //Rotate a vector 90 degrees to the left
         return Quaternion.LookRotation(Vector3.forward, relativeUpDir) * scaledParabolicPos;
     }
 }
